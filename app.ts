67bebd9989c8b336b2d075cedae0cea4f1081948
baio <max.putilov@gmail.com>
2015-06-18 11:55:32 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -66,6 +66,17 @@ var list : IList = {
   pairs :
   [
     {
+      date : "18.06.2015",
+      there : {
+        text : "Мэр Лондона обматерил таксиста", 
+        link: "http://www.dailymail.co.uk/news/article-3128665/F-die-not-order-Boris-told-irate-cabbie-late-night-street-row-taxi-app-Uber-cycled-home-dinner.html", 
+        image : "http://i.dailymail.co.uk/i/pix/2015/06/17/20/29B7445C00000578-0-image-a-122_1434568719557.jpg"},
+      here : {
+        text : "Губернатор заказал молебен о стабильности", 
+        link: "http://www.znak.com/urfo/news/2015-06-17/1041402.html", 
+        image : "http://www.znak.com/images/news/201410241156/201506171624/moleben.jpg"}
+    },        
+    {
       date : "17.06.2015",
       there : {
         text : "Представлен новый Kindle Paper White", 
